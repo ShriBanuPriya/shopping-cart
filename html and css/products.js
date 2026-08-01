a265ef8677d3c6395d090fcfe6558564825fa6a6
ShriBanuPriya <priyashribanu@gmail.com>
2026-08-01 14:01:06 +0530

    fix path changes

--- html and css/products.js
+++ html and css/products.js
@@ -3,21 +3,21 @@
     id: 1,
     name: "Wooden Toy Car",
     price: 299,
-    image: "images/car.jpg",
+    image: "/images/car.jpg",
     description: "Eco-friendly wooden toy car for kids."
   },
   {
     id: 2,
     name: "Wooden Doll",
     price: 499,
-    image: "images/doll.jpg",
+    image: "/images/doll.jpg",
     description: "Handmade wooden doll."
   },
   {
     id: 3,
     name: "Wooden Train",
     price: 799,
-    image: "images/train.jpg",
+    image: "/images/train.jpg",
     description: "Colorful wooden train set."
   }
 ];
