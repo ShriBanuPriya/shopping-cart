09fe92fa714dbf1a251ae151296c774276b0d2b6
ShriBanuPriya <priyashribanu@gmail.com>
2026-08-01 13:42:59 +0530

    image path changes

--- html and css/productsp.js
+++ html and css/productsp.js
@@ -4,42 +4,42 @@ const products = [
     id: 1,
     name: "Wooden Car",
     price: 299,
-    image: "../images/doll1.jpg",
+    image: "/images/doll1.jpg",
     description: "Eco-friendly toy"
   },
   {
     id: 2,
     name: "Wooden Doll",
     price: 499,
-    image: "../images/doll2.jpg",
+    image: "/images/doll2.jpg",
     description: "Handmade doll"
   },
    {
     id: 3,
     name: "Wooden Doll",
     price: 200,
-    image: "../images/doll7.jpg",
+    image: "/images/doll7.jpg",
     description: "Tiny Timber Tots"
   },
    {
     id: 4,
     name: "Wooden Doll",
     price: 400,
-    image: "../images/doll9.jpg",
+    image: "/images/doll9.jpg",
     description: "Woody Whimsy"
   },
    {
     id: 5,
     name: "Wooden Doll",
     price: 250,
-    image: "../images/doll11.jpeg",
+    image: "/images/doll11.jpeg",
     description: "Oakie Dokie Doll"
   },
    {
     id: 6,
     name: "Wooden Doll",
     price: 450,
-    image: "../images/doll6.jpg",
+    image: "/images/doll6.jpg",
     description: "Artisian Ash Doll"
   },
 
